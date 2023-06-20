a5a2c799ae2f9752a6d7f28c9d799e78b91a84aa
blueslaboratory <myjunkandspam22@gmail.com>
2023-06-20 09:14:59 +0200

    day 16

--- 012 Avanzados/Ej001 Partes3/ln/errors/Ej001_ErrorsLN.py
+++
@@ -1,9 +0,0 @@
-# Day 15 - 19/06/2023
-# Ejercicios Mix
-
-# Esta clase ya tiene herencia de excepcion
-# hereda de la clase en parentesis, en este caso Exception
-
-class ErrorLN(Exception):
-	# pass: palabra reservada para decir que ya lo implementaras
-	pass
